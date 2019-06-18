9ce33603d1da67ac2349de7c12fec2097e161443
m-adrien <mchich.adrien@gmail.com>
2019-06-18 11:59:28 +0200

    Interface1OK
    1er jet  sans vérification de la première partie sur les interfaces OK

--- main.py
+++ main.py
@@ -1,24 +1,15 @@
 #!/usr/bin/python3.5
 # -*-coding:utf-8 -*
 
-#Création des argument options :
+# ---------- Module | Conf.ini | Options ---------- #
+
+# Import argparse pour les options
 import argparse
-parser = argparse.ArgumentParser()
-parser.parse_args()
-
-####Vérifions que nous sommes sur Debian####
-
-##Création -> variable verif -> présence du mot debian dans la version de la distribution##
-#Import commandes bash de l'os#
+# Import commandes bash de l'os
 import os
-os.system('uname -a | grep Debian')
-verif = os.system('echo $?')
-#Si Verif = 1 = pas sur Debian = On arrête le programme
-if verif == 1:
-   sys.exit([0])
-
-#On importe la configuration et on recupère nos variables
+# On importe la configuration
 import configparser
+# On recupère nos variables du fichier .ini
 config = configparser.ConfigParser()
 config.read('conf.ini')
 IP1 = config.get('INTERFACES', 'IP1')
@@ -46,34 +37,41 @@ DGATE1 = config.get('DHCP', 'DGATE1')
 DGATE2 = config.get('DHCP', 'DGATE2')
 DGATE3 = config.get('DHCP', 'DGATE3')
 
-##Verifications et la conf OK --> C'est parti ! ##
-#Configuration des interfaces
+# Création des argument options :
+parser = argparse.ArgumentParser()
+parser.parse_args()
 
+# -----------Vérifions que nous sommes sur Debian----------- #
+
+# Création -> variable verif -> présence du mot debian dans la version de la distribution
+
+os.system('uname -a | grep Debian')
+verif = os.system('echo $?')
+# Si Verif = 1 = pas sur Debian = On arrête le programme
+if verif == 1:
+   sys.exit([0])
+# ----Verifications OK + Conf Chargé --> C'est parti !---- #
+
+# -----------Configuration des interfaces-----------#
+
+# Ouverture et ecriture (erase) du fichier de conf
 interfaces = open("/etc/network/interfaces", "w")
-#Importation du module pickle
-import pickle
-#Création du pickle de conf de base
-iface0 = "source /etc/network/interfaces.d/*\n\n#loopback iface\nauto lo\niface lo inet loopback\n\n"
-with open('iface0', 'wb') as fichier:
-    mon_pickler = pickle.Pickler(fichier)
-    mon_pickler.dump(score)
+# Insertion de la conf de base
+interfaces.write("source /etc/network/interfaces.d/*\n\n#loopback iface\nauto lo\niface lo inet loopback\n\n")
+# Insertion de la conf de l'iface 1
+interfaces.write("#Iface 1\nauto enp0s3\niface enp0s3 inet static\naddress {}\nnetmask {}\ngateway {}\n\n"
+                 .format(IP1, NM1, GW1))
+# Insertion de la conf de l'iface 2
+interfaces.write("#Iface 2\nauto enp0s3\niface enp0s3 inet static\naddress {}\nnetmask {}\ngateway {}\n\n"
+                 .format(IP2, NM2, GW2))
+# Insertion de la conf de l'iface 3
+interfaces.write("#Iface 3\nauto enp0s3\niface enp0s3 inet static\naddress {}\nnetmask {}\ngateway {}\n\n"
+                 .format(IP3, NM3, GW3))
+# Fermeture du fichier
+interfaces.close()
 
-#Création du pickle "iface1" de conf d'interfaces 1
-iface1 = "#Iface 1\nauto enp0s3\niface enp0s3 inet static\naddress {}\nnetmask {}\ngateway {}\n\n".format(IP1, NM1, GW1)
-with open('iface1', 'wb') as fichier:
-    mon_pickler = pickle.Pickler(fichier)
-    mon_pickler.dump(score)
+# --- Configuration interface terminée --- #
 
-#Création du pickle "iface2" de conf d'interfaces 2
-iface2 = "#Iface 2\nauto enp0s3\niface enp0s3 inet static\naddress {}\nnetmask {}\ngateway {}\n\n".format(IP2, NM2, GW2)
-with open('iface2', 'wb') as fichier:
-    mon_pickler = pickle.Pickler(fichier)
-    mon_pickler.dump(score)
 
-#Création du pickle "iface3" de conf d'interfaces 3
-iface3 = "#Iface 3\nauto enp0s3\niface enp0s3 inet static\naddress {}\nnetmask {}\ngateway {}\n\n".format(IP3, NM3, GW3)
-with open('iface3', 'wb') as fichier:
-    mon_pickler = pickle.Pickler(fichier)
-    mon_pickler.dump(score)
 
-#Ramplacer le fichier interface par nos valeurs
+
