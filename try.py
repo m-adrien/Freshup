e4ff846a5e8773e5a2dcf3c43b098fc4d0e045c9
m-adrien <mchich.adrien@gmail.com>
2019-07-02 20:52:58 +0200

    Correction forceDHCP et INPUT IMAP

--- try.py
+++ try.py
@@ -1,9 +1,25 @@
-errorX = None
-try:
-    a1 = True
-    print('print this will not work{}'.format(a1))
-except Exception as erreur:
-    errorX = erreur
+import json
+data = open('conf.json', 'r')
+config = json.load(data)
+data.close()
 
-if errorX:
-    print('This is working')
+if config['Interfaces']['eth0']['Type'] == 'LAN' or config['DHCP']['eth0']['Forced'] == 1:
+    dhcpeth0 = 1  # Création de la variable pour la suite
+    print("subnet {} netmask {} {{\n  range {} {};\n  option routers {};\n}}\n\n".format(
+    config['DHCP']['eth0']['SubNet'], config['DHCP']['eth0']['NetMask'],
+    config['DHCP']['eth0']['Start'], config['DHCP']['eth0']['End'],
+    config['DHCP']['eth0']['Dgate']))
+
+if config['Interfaces']['eth1']['Type'] == 'LAN' or config['DHCP']['eth1']['Forced'] == 1:
+    dhcpeth1 = 1  # Création de la variable pour la suite
+    print("subnet {} netmask {} {{\n  range {} {};\n  option routers {};\n}}\n\n".format(
+        config['DHCP']['eth1']['SubNet'], config['DHCP']['eth1']['NetMask'],
+        config['DHCP']['eth1']['Start'], config['DHCP']['eth1']['End'],
+        config['DHCP']['eth1']['Dgate']))
+
+if config['Interfaces']['eth2']['Type'] == 'LAN' or config['DHCP']['eth2']['Forced'] == 1:
+    dhcpeth2 = 1  # Création de la variable pour la suite
+    print("subnet {} netmask {} {{\n  range {} {};\n  option routers {};\n}}\n\n".format(
+        config['DHCP']['eth2']['SubNet'], config['DHCP']['eth2']['NetMask'],
+        config['DHCP']['eth2']['Start'], config['DHCP']['eth2']['End'],
+        config['DHCP']['eth2']['Dgate']))
